7bf84a5a0813b7664661671e99a4691c6ace4b26
JackEdwardLyons <jack.edward.lyons@gmail.com>
2019-07-07 22:03:25 -0600

    Change avatar image input to file upload

--- src/components/FormField.vue
+++ src/components/FormField.vue
@@ -1,7 +1,35 @@
 <template>
   <div class="mb-4">
-    <label class="block text-gray-700 text-sm font-bold mb-2" :for="id">{{ label }}</label>
-    <input @input="$emit('input', $event.target.value)" :value="value" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" :id="id" :type="type" :placeholder="placeholder" required />
+    <div v-if="type !== 'file'">
+      <label class="block text-gray-700 text-sm font-bold mb-2" :for="id">{{ label }}</label>
+      <input
+        @input="$emit('input', $event.target.value)"
+        :value="value"
+        class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        :id="id"
+        :type="type"
+        :placeholder="placeholder"
+        required
+      />
+    </div>
+    
+    <div v-else>
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="upload-input">Avatar Image</label>
+      <div class="flex" :class="{ 'justify-around': uploadFile.image }">
+        <input 
+          id="upload-input" 
+          type="file" 
+          @change.prevent="onFileChange" 
+          accept="image/*" 
+        />
+        <img
+          v-show="uploadFile.image"
+          class="rounded-full w-12 h-12 shadow-md upload-img"
+          :src="uploadFile.image"
+          alt="Your uploaded avatar image"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,20 +45,43 @@ export default {
     },
     type: {
       type: String,
-      default: 'text'
+      default: "text"
     },
     id: {
       type: String,
-      default: 'field__' + Math.floor(Math.random() * 100)
+      default: "field__" + Math.floor(Math.random() * 100)
     },
     placeholder: {
       type: String,
-      default: 'Please fill in this field.'
+      default: "Please fill in this field."
+    }
+  },
+  data() {
+    return {
+      uploadFile: {
+        name: '',
+        image: ''
+      }
+    }
+  },
+  methods: {
+    onFileChange (e) {
+      let files = e.target.files || e.dataTransfer.files
+      if (!files.length) return
+      return this.createImage(files[0])
+    },
+    createImage: function(file) {
+      let reader = new FileReader()
+      this.uploadFile.name = file.name
+
+      reader.onload = e => {
+        this.uploadFile.image = e.target.result
+        this.$emit('input', this.uploadFile.image)
+      }
+
+      return reader.readAsDataURL(file)
     }
   }
 }
 </script>
 
-<style>
-
-</style>
--- src/components/InputCard.vue
+++ src/components/InputCard.vue
@@ -6,10 +6,10 @@
     <form-field class="mb-4" v-model="occupation" label="Occupation" placeholder="Your Occupation" />
     <form-field class="mb-4" v-model="email" label="Email" type="email" placeholder="Your Best Email" />
     <form-field class="mb-4" v-model="phone" label="Phone" type="tel" placeholder="Your Best Phone Number" />
-    <form-field class="mb-4" v-model="avatarImg" label="Avatar Image" type="text" placeholder="Your Avatar Image URL" />
+    <form-field class="mb-4" v-model="avatarImg" label="Avatar Image" type="file" />
 
-    <div class="flex items-center justify-between">
-      <a class="inline-block align-baseline font-bold text-sm text-red-500 hover:text-red-800" href="#">
+    <div class="flex items-center justify-between mt-8">
+      <a @click="clearFields" class="inline-block align-baseline font-bold text-sm text-red-500 hover:text-red-800" href="#">
         Clear All
       </a>
       <button @click="submitUser" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
@@ -33,7 +33,7 @@ export default {
       company:  '',
       email: '',
       phone: null,
-      avatarImg: ''
+      avatarImg: undefined,
     }
   },
   methods: {
@@ -46,7 +46,15 @@ export default {
         phone: this.phone,
         avatarImg: this.avatarImg
       })
+    },
+    clearFields () {
+      this.name = ''
+      this.occupation = ''
+      this.company =  ''
+      this.email = ''
+      this.phone = null
+      this.avatarImg = ''
     }
   }
-};
+}
 </script>
